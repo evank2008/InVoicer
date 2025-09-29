eddb19fc168ca366309bacc13a782b1c235bd957
Evan Keri <Evan Keri@172.23.133.132>
2025-09-29 11:21:30 -0700

    added some ui basics

--- InVoicer/src/InVoicer.java
+++ InVoicer/src/InVoicer.java
@@ -1,3 +1,7 @@
+import java.awt.Color;
+
+import javax.swing.*;
+
 public class InVoicer {
 	
 	//this program will create a window where you can input and view
@@ -11,6 +15,26 @@ public class InVoicer {
 	//when making a new invoice give a lil dropdown menu of all past clients or 
 	//an entry text box
 	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		frame.setLayout(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//one panel for controls, one for display
+		JPanel controlPanel = new JPanel();
+		JButton addB = new JButton("Create");
+		controlPanel.add(addB);
+		JButton editB = new JButton("Edit");
+		controlPanel.add(editB);
+			controlPanel.setSize(600, 80);
+			controlPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+			frame.add(controlPanel);
+		JPanel displayPanel = new JPanel();
+		displayPanel.setSize(600, 520);
+		displayPanel.setBorder(BorderFactory.createLineBorder(Color.red));
+		frame.add(displayPanel);
+
+		
+		frame.setSize(600,640);
 		
 	}
 }
--- InVoicer/src/Invoice.java
+++ InVoicer/src/Invoice.java
@@ -5,8 +5,20 @@ public class Invoice {
 //store data about date history, price, client, paid/unpaid, etc
 	LocalDateTime dateCreated;
 	LocalDateTime datePaid;
+	LocalDateTime lastUpdated;
 	String client;
 	double amount;
 	boolean paidStatus;
-	
+	public Invoice() {
+		dateCreated=LocalDateTime.now();
+		paidStatus=false;
+	}
+	void pay() {
+		paidStatus=true;
+		datePaid=LocalDateTime.now();
+		update();
+	}
+	void update() {
+		lastUpdated=LocalDateTime.now();
+	}
 }
