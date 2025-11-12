ecda130e372b705e28ba03938919fc3ae827173a
Evan Keri <Evan Keri@DESKTOP-2O56U0E.lan>
2025-11-12 09:40:15 -0800

    added client framework via declaring a lot of variables

--- InVoicer/src/inv/ClientPanel.java
+++ InVoicer/src/inv/ClientPanel.java
@@ -1,11 +1,44 @@
 package inv;
 
-import javax.swing.JLabel;
+import java.util.ArrayList;
+
+import javax.swing.*;
 
 //this class should display the clients, let you add/remove clients, input data about clients
+//have a row of clientboxes on the left side
+//buttons on the right side?
 public class ClientPanel extends MenuPanel {
 	public ClientPanel() {
 		super();
 		add(new JLabel("Client Panel"));
+	}
+}
+
+class Client {
+	String name;
+	static ArrayList<String> doctorList = new ArrayList<String>();
+	int doctor; //position in list
+	double expectedAmt; //expected monthly billing amount in dollars
+	ArrayList<Contact> contactList = new ArrayList<Contact>();
+
+}
+class Contact {
+	String name;
+	String emailAddress;
+	String role="";
+	public Contact(String name, String address) {
+		this.name=name;
+		emailAddress=address;
+	}
+	public Contact(String name, String address, String role) {
+		this(name, address);
+		this.role=role;
+	}
+}
+class ClientBox extends JPanel{
+	Client client;
+	public ClientBox(Client client) {
+		super();
+		this.client=client;
 	}
 }
--- InVoicer/src/inv/MenuPanel.java
+++ InVoicer/src/inv/MenuPanel.java
@@ -4,9 +4,7 @@ import javax.swing.*;
 
 //this class is a parent of records, clients, and creator
 public abstract class MenuPanel extends JPanel {
-
-	// want a back button in the top right corner
-	// takes you back to main screen
+//TODO: add a simon button in the top right?
 	public MenuPanel() {
 		super();
 		add(new JLabel("Menu Panel"));
