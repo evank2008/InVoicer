18dc5d8c8f4044a354a60848b9436e09a901b924
Evan Keri <Evan Keri@DESKTOP-2O56U0E.sdcs.local>
2025-12-05 11:10:09 -0800

    added button to generate pdf. doesn't yet do that but it does take the data

--- InVoicer/src/inv/CreatorPanel.java
+++ InVoicer/src/inv/CreatorPanel.java
@@ -1,6 +1,8 @@
 package inv;
 
 import java.awt.*;
+import java.time.LocalDate;
+import java.util.Date;
 
 import javax.swing.*;
 
@@ -21,8 +23,10 @@ import com.github.lgooddatepicker.components.DatePicker;
 public class CreatorPanel extends MenuPanel {
 	DatePicker serviceDatePicker, billDatePicker;
 	JLabel clientLabel, serviceDateLabel, billDateLabel, serviceFieldLabel, amountLabel;
+	JLabel errorLabel, successLabel;
 	JTextField serviceField;
 	JTextField amountField;
+	JButton generateButton;
 	//two date pickers
 	JComboBox<Client> clientPicker;
 	public CreatorPanel() {
@@ -71,16 +75,78 @@ public class CreatorPanel extends MenuPanel {
 		add(bufferPanel());
 		
 		serviceDatePicker = new DatePicker();
+		serviceDatePicker.setMaximumSize(new Dimension(Invoicer.WIDTH*8/10,Invoicer.HEIGHT/20));
+		serviceDateLabel = new JLabel("Service Date");
+		serviceDateLabel.setForeground(Color.white);
+		serviceDateLabel.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, Invoicer.HEIGHT/30));
+		
+		add(serviceDateLabel);
 		add(serviceDatePicker);
+		add(bufferPanel());
+		
+		billDatePicker = new DatePicker();
+		billDatePicker.setMaximumSize(new Dimension(Invoicer.WIDTH*8/10,Invoicer.HEIGHT/20));
+		billDatePicker.setDateToToday();
+		billDateLabel = new JLabel("Billing Date");
+		billDateLabel.setForeground(Color.white);
+		billDateLabel.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, Invoicer.HEIGHT/30));
+		
+		add(billDateLabel);
+		add(billDatePicker);
+		add(bufferPanel());
+		add(bufferPanel());
+		
+		generateButton = new JButton("Generate PDF");
+		if(Invoicer.onMac) {
+			generateButton.setForeground(Color.black);
+		} else {
+			generateButton.setForeground(Color.white);
+		}
+		generateButton.setBackground(new Color(40,160,230));
+		generateButton.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,Invoicer.HEIGHT/20));
+		generateButton.addActionListener(e->{
+			//Client client, String service, double amount, Date serviceDate, Date billDate
+			try {
+				errorLabel.setVisible(false);
+				successLabel.setVisible(false);
+				Client client = (Client) clientPicker.getSelectedItem();
+				String service = serviceField.getText();
+				double amount = Double.parseDouble((amountField.getText()));
+				LocalDate serviceDate = serviceDatePicker.getDate();
+				if(serviceDate==null||amount<0||service.equals("")) throw new Exception();
+				LocalDate billDate = billDatePicker.getDate();
+				
+				generatePDF(client,service,amount,serviceDate,billDate);
+			} catch (Exception ex) {
+				errorLabel.setVisible(true);
+			}
+		});
+		generateButton.setPreferredSize(new Dimension(Invoicer.WIDTH*8/10,Invoicer.HEIGHT/10));
+		add(generateButton);
+		
+		errorLabel = new JLabel("Some fields not filled out properly");
+		errorLabel.setForeground(Color.red);
+		errorLabel.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, Invoicer.HEIGHT/35));
+
+		add(bufferPanel());
+		add(errorLabel);
+		
+		successLabel = new JLabel("Success! TODO make this say file save loc");
+		successLabel.setForeground(Color.green);
+		successLabel.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, Invoicer.HEIGHT/35));
+
+		add(bufferPanel());
+		add(successLabel);
+		
+		successLabel.setVisible(false);
+		errorLabel.setVisible(false);
+
 	}
 	public void updateClientPicker() {
 		((DefaultComboBoxModel<Client>) clientPicker.getModel()).removeAllElements();
 		for(ClientBox c: ClientPanel.clientList) {
-			System.out.println(c.client);
-			System.out.println(ClientPanel.clientList.size());
 			((DefaultComboBoxModel<Client>) clientPicker.getModel()).addElement(c.client);
 		}
-		
 	}
 	JPanel bufferPanel() {
 		JPanel bufferPanel = new JPanel();
@@ -88,6 +154,12 @@ public class CreatorPanel extends MenuPanel {
 		bufferPanel.setMaximumSize(new Dimension(Invoicer.WIDTH*6/10,Invoicer.HEIGHT/40));
 		bufferPanel.setBackground(new Color(36,36,36));
 		return bufferPanel;
+	}
+	void generatePDF(Client client, String service, double amount, LocalDate serviceDate, LocalDate billDate) {
+		//TODO this
+		
+		//on success:
+		successLabel.setVisible(true);
 	}
 }
 
