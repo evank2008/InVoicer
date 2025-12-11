96d2fa94afe04763764745bcd3de709aa8e629af
Evan Keri <Evan Keri@DESKTOP-2O56U0E.sdcs.local>
2025-12-10 20:14:44 -0800

    saving file, now with error message stack trace

--- InVoicer/src/inv/Invoicer.java
+++ InVoicer/src/inv/Invoicer.java
@@ -7,6 +7,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileSystemView;
@@ -15,6 +16,7 @@ import javax.swing.filechooser.FileSystemView;
 //		add file saving/reading system
 //		maybe just make it save on close? with the jframe
 public class Invoicer {
+	String errorMsg="";
 	static JFrame frame;
 	static ClientPanel clp;
 	static CreatorPanel crp;
@@ -62,26 +64,19 @@ public class Invoicer {
 				System.exit(0);
 			}
 		});
-		
+		loadFile();
 	}
 	void saveAllData() {
 		ClientPanel.updateClientData();
-		//TODO: make this save to files etc
-		//also probably update the data for the other two panels when you get around to them
-		//cant really write the data aving code until all the data fields are set up anyway
-		//no data to save for creatorpanel anyway
-		//ClientPanel.clientList
-		//RecordsPanel.recordsList
 		//commas should separate things within categories
 		//should be the same amount of commas for each bracket i think? is that wrong
 		//split repeatable things with <>s
 		//take clients for instance you would first split by <client>
 		//a client's tofilestring is name,doctor,amount<contactList>contactstuffwithcommas separated by <contact>
 		//first split by <contactList> then split first one by commas and the second by <contact> and then by commas, system works
-		System.out.println(clp.toFileString());
-		System.out.println(rp.toFileString());
 
 		BufferedWriter bw;
+		
 		try {
 			bw = new BufferedWriter(new FileWriter(
 					new File(FileSystemView.getFileSystemView().getDefaultDirectory().getPath() + "/invoicerData.txt")));
@@ -91,10 +86,17 @@ public class Invoicer {
 				bw.close();
 				System.out.println("saved successfully");
 		} catch (IOException e) {
-			JOptionPane.showMessageDialog(null, "Error occurred while saving.");
+			Arrays.asList(e.getStackTrace()).forEach(j->errorMsg+=j.toString()+"\n");
+			JOptionPane.showMessageDialog(null, "Error occurred while saving: "+errorMsg);
 			e.printStackTrace();
 		}
 	
 	}
-
+	void loadFile() {
+		String path=FileSystemView.getFileSystemView().getDefaultDirectory().getPath()+ "/invoicerData.txt";
+		File dataFile = new File(path);
+		if(!dataFile.exists()) {
+			JOptionPane.showMessageDialog(null,"Welcome to the Invoicer! Your data file will be saved to "+path+". Enjoy!");
+		}
+	}
 }
