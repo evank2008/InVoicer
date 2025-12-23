94f215a7a37f21d1cdb600a3c4217b66f4d60918
Evan Keri <Evan Keri@DESKTOP-2O56U0E.lan>
2025-12-22 20:40:45 -0800

    made the pdf generator work. not yet hooked up to the rest of the code but we'll get to that.

--- InVoicer/src/inv/PDFGenerator.java
+++ InVoicer/src/inv/PDFGenerator.java
@@ -1,8 +1,30 @@
 package inv;
 
+import java.awt.Font;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import javax.swing.filechooser.FileSystemView;
+import javax.swing.text.DateFormatter;
+
+import com.itextpdf.io.font.FontConstants;
+import com.itextpdf.io.font.PdfEncodings;
+import com.itextpdf.kernel.color.Color;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.border.Border;
+import com.itextpdf.layout.border.SolidBorder;
+import com.itextpdf.layout.element.Cell;
+import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.property.HorizontalAlignment;
+import com.itextpdf.layout.property.TextAlignment;
+import com.itextpdf.layout.property.VerticalAlignment;
 
 
 
@@ -11,12 +33,12 @@ public class PDFGenerator {
 	//below that, invoice genration date
 	public static void main(String[] args) {
 		Client c = new Client();
-		c.name="John M. Client";
+		c.name="J. P. Client Co.";
 		Contact jim = new Contact("Jim Contact","jim@client.net","Director of Jim");
 		Contact clark = new Contact("Clark Contact","clark@client.net","Beer Drinker");
 		c.contactList.add(jim);
 		c.contactList.add(clark);
-		generatePdf(c,"Stabbing",500,20,LocalDate.EPOCH,LocalDate.now());
+		generatePdf(c,"Stabbing",500,20.5,LocalDate.EPOCH,LocalDate.now());
 	}
 	public static boolean generatePdf(Client client, String service, double amount, double hourly, LocalDate serviceDate, LocalDate billDate){
 		//TODO figure this one out
@@ -32,8 +54,93 @@ public class PDFGenerator {
 		 * total amount
 		 * 
 		 */
+		String invoiceNum = LocalDate.now().getYear()+"-"+LocalDate.now().getMonthValue(); //2025-12
 		String path = FileSystemView.getFileSystemView().getDefaultDirectory().getPath()+ "/invoice.pdf";
-		
+		try {
+			PdfWriter writer = new PdfWriter(path);
+			PdfDocument pDoc = new PdfDocument(writer);
+			Document doc = new Document(pDoc);
+			
+			PdfFont roman = PdfFontFactory.createFont(FontConstants.TIMES_ROMAN, PdfEncodings.MACROMAN);
+			
+			pDoc.setDefaultPageSize(PageSize.LETTER);
+			
+			Table table = new Table(new float[] {300,250});//header
+			table.addCell(new Cell().add("Jason Keri MD").setBold().setFontSize(24).setBorder(Border.NO_BORDER));
+			table.addCell(new Cell().setBorder(Border.NO_BORDER));
+			table.addCell(new Cell().add("Geriatric Psychiatry").setItalic().setBold().setFontSize(24).setBorder(Border.NO_BORDER));
+			table.addCell(new Cell().add("INVOICE\n"+invoiceNum).setTextAlignment(TextAlignment.RIGHT).setBorder(Border.NO_BORDER).setFont(roman));
+			table.addCell(new Cell().add("8677 Villa La Jolla Dr #205\nLa Jolla, CA 92037\n(619) 299-4374\nJK@SeniorMedicalAssociates.com").setBold().setHeight(80).setBorder(Border.NO_BORDER));
+			table.addCell(new Cell().add(LocalDate.now().format(DateTimeFormatter.ofPattern("uuuu/MM/d"))).setBorder(Border.NO_BORDER).setTextAlignment(TextAlignment.RIGHT).setVerticalAlignment(VerticalAlignment.BOTTOM));
+			doc.add(table);
+			
+			Table contactTable=new Table(new float[] {500,500});
+			contactTable.setBorder(new SolidBorder(Color.BLACK,1));
+			//contactTable.addCell(new Cell().add("text").setBorder(Border.NO_BORDER));
+			contactTable.addCell(new Cell().add("To:").setBorder(Border.NO_BORDER));
+			contactTable.addCell(new Cell().add("For: "+client.name).setBorder(Border.NO_BORDER));
+			for(Contact con:client.contactList) {
+				contactTable.addCell(new Cell().add(con.name+"\n"+con.emailAddress).setBorder(Border.NO_BORDER).setHeight(50));
+				contactTable.addCell(new Cell().setBorder(Border.NO_BORDER));
+			}
+			doc.add(contactTable);
+			
+			Table bodyTable = new Table(new float[] {200,700,300});
+			
+			bodyTable.addCell(new Cell().add("").setHeight(20).setBorder(Border.NO_BORDER));
+			bodyTable.addCell(new Cell().add("").setHeight(20).setBorder(Border.NO_BORDER));
+			bodyTable.addCell(new Cell().add("").setHeight(20).setBorder(Border.NO_BORDER));
+			
+			bodyTable.addCell(new Cell().add("Date").setTextAlignment(TextAlignment.CENTER).setFont(roman));
+			bodyTable.addCell(new Cell().add("Description").setTextAlignment(TextAlignment.CENTER).setFont(roman));
+			bodyTable.addCell(new Cell().add("Amount").setTextAlignment(TextAlignment.CENTER).setFont(roman));
+			
+			bodyTable.addCell(new Cell().add("").setHeight(50));
+			bodyTable.addCell(new Cell().add("").setHeight(50));
+			bodyTable.addCell(new Cell().add("").setHeight(50));
+
+			bodyTable.addCell(new Cell().add(serviceDate.format(DateTimeFormatter.ofPattern("MMM uuuu"))));
+			bodyTable.addCell(new Cell().add(service));
+			bodyTable.addCell(new Cell().add(""));
+			
+			bodyTable.addCell(new Cell().add("").setHeight(bodyTable.getCell(0, 0).getHeight()));
+			bodyTable.addCell(new Cell().add(""));
+			bodyTable.addCell(new Cell().add(""));
+			
+			if(hourly>0) {
+			bodyTable.addCell(new Cell().add(""));
+			bodyTable.addCell(new Cell().add("Hourly Rate").setBold().setTextAlignment(TextAlignment.RIGHT));
+			
+			String formatted;
+			if (hourly == Math.floor(hourly)) {
+			    formatted = String.valueOf((int) hourly);
+			} else {
+			    formatted = String.format("%.2f", hourly);
+			}
+			
+			bodyTable.addCell(new Cell().add("$"+formatted));
+			
+			}
+			
+			bodyTable.addCell(new Cell());
+			
+			
+			bodyTable.addCell(new Cell().add("TOTAL").setTextAlignment(TextAlignment.RIGHT).setFont(roman));
+			String formatted;
+			if (amount == Math.floor(amount)) {
+			    formatted = String.valueOf((int) amount);
+			} else {
+			    formatted = String.format("%.2f", amount);
+			}
+			bodyTable.addCell(new Cell().add("$"+formatted));
+
+			doc.add(bodyTable);
+			
+			doc.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return true;
 	}
 }
