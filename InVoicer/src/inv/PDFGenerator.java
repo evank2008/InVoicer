66136d77b391f64997792252b4e7d155bcfb119e
Evan Keri <Evan Keri@DESKTOP-2O56U0E.lan>
2025-12-22 18:50:56 -0800

    started work on pdf. updated pdf version in pom

--- InVoicer/src/inv/PDFGenerator.java
+++ InVoicer/src/inv/PDFGenerator.java
@@ -2,9 +2,22 @@ package inv;
 
 import java.time.LocalDate;
 
+import javax.swing.filechooser.FileSystemView;
+
+
+
 public class PDFGenerator {
 //top right number is 'invoice number' which can just be year+month of service date
 	//below that, invoice genration date
+	public static void main(String[] args) {
+		Client c = new Client();
+		c.name="John M. Client";
+		Contact jim = new Contact("Jim Contact","jim@client.net","Director of Jim");
+		Contact clark = new Contact("Clark Contact","clark@client.net","Beer Drinker");
+		c.contactList.add(jim);
+		c.contactList.add(clark);
+		generatePdf(c,"Stabbing",500,20,LocalDate.EPOCH,LocalDate.now());
+	}
 	public static boolean generatePdf(Client client, String service, double amount, double hourly, LocalDate serviceDate, LocalDate billDate){
 		//TODO figure this one out
 		//ingredients that are variable:
@@ -19,6 +32,8 @@ public class PDFGenerator {
 		 * total amount
 		 * 
 		 */
+		String path = FileSystemView.getFileSystemView().getDefaultDirectory().getPath()+ "/invoice.pdf";
+		
 		return true;
 	}
 }
